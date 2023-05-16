1271055e220add2461d5fc7b5ae3bc1245d84d33
H3BCKN <ka.koszewski@gmail.com>
2023-05-17 01:37:16 +0200

    drawing board

--- tetris.rb
+++ tetris.rb
@@ -6,6 +6,40 @@ GRID = 50
 BACKGROUND_COLOR = '#000000'
 
 class Tetromino
+  attr_accessor :alive
+  attr_reader :color, :coordinates
+
+  def initialize 
+    @color = random_color
+    @coordinates = random_shape
+    @alive = true
+  end
+
+  def move(speed = 1)
+  end
+
+  def rotate
+  end
+
+  private
+  def random_color
+    ['#0341AE', '##72CB3B', '##FFD500', '#FF971C', '#FF3213'].shuffle 
+  end
+
+  def random_shape
+    [
+      [[2,0].[2,1],[2,2],[2,3]], # I
+      [[1,0],[1,1],[2,1],[3,1]], # J 
+      [[3,0],[3,1],[2,1],[1,1]], # L 
+      [[1,0],[1,1],[2,0],[2,1]], # O
+      [[2,0],[3,0],[1,1],[2,1]], # S 
+      [[2,0],[1,1],[2,1],[3,1]], # T 
+      [[1,0],[2,0],[2,1],[3,1]], # Z
+  ].shuffle
+  end 
+
+  def random_position 
+  end 
 end
 
 class Game
@@ -13,7 +47,9 @@ class Game
 
   def initialize
     @tetrominos = []
-    generate_tetromino
+  end
+
+  def draw
   end
 
   private 
@@ -22,7 +58,10 @@ class Game
   end
 end
 
-game = Game.new 
+# game = Game.new 
+t = Tetromino.new 
+p t.color
+p t.coordinates
 
 set width: WIDTH
 set height: HEIGHT
